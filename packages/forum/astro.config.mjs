23c5f642d00ff491ac978d42993497f106840634
Soc Virnyl Estela <soc.estela@tribex.co>
2024-08-24 01:57:32 +0000

    netlify-edge: enable edge middleware

--- packages/forum/astro.config.mjs
+++ packages/forum/astro.config.mjs
@@ -4,7 +4,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "server",
   adapter: netlify({
     edgeMiddleware: true
   })
