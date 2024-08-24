975a0e2028d3a47dc8a0456c87853df2cacb7baf
Soc Virnyl Estela <soc.estela@tribex.co>
2024-08-24 03:48:03 +0000

    restart: welp idk much astro yet so let's try from scratch again

--- packages/forum/astro.config.mjs
+++ packages/forum/astro.config.mjs
@@ -1,11 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: netlify({
-    edgeMiddleware: false
-  })
-});
+export default defineConfig({});
--- packages/forum/src/env.d.ts
+++ packages/forum/src/env.d.ts
@@ -1,9 +1,1 @@
 /// <reference path="../.astro/types.d.ts" />
-
-type NetlifyLocals = import("@astrojs/netlify").NetlifyLocals
-
-declare namespace App {
-    interface Locals extends NetlifyLocals {
-        
-    }
-}
