1e3ef53d62a8b11822eec7958e5b028683591c65
Soc Virnyl Estela <soc.estela@tribex.co>
2024-08-28 10:40:52 +0000

    format

--- packages/foundry/test/Forum.t.sol
+++ packages/foundry/test/Forum.t.sol
@@ -18,25 +18,25 @@ contract ForumTest is Test {
         forum.createPost(_title, _description, _spoil);
     }
 
-	function testCreatePoll() public {
-		forum.createPoll(0, "Is StackUp The Best?", "Yes", "No");
-		Forum.Poll memory _poll = forum.getPollFromPost(0);
-		assertEq(_poll.question, "Is StackUp The Best?");
-	}
+    function testCreatePoll() public {
+        forum.createPoll(0, "Is StackUp The Best?", "Yes", "No");
+        Forum.Poll memory _poll = forum.getPollFromPost(0);
+        assertEq(_poll.question, "Is StackUp The Best?");
+    }
 
-    function testGetPost() public {
-		Forum.Post memory _post = forum.getPost(0);
+    function testGetPost() public view {
+        Forum.Post memory _post = forum.getPost(0);
         assertEq(_post.title, "StackUp");
         assertEq(_post.description, "Empowering Developers at Scale!");
         assertEq(_post.spoil, false);
     }
 
-	function testVotePost() public {
-		forum.upVotePost(0);
-		Forum.Post memory _post = forum.getPost(0);
-		assertEq(_post.likes, 1);
-		forum.downVotePost(0);
-		_post = forum.getPost(0);
-		assertEq(_post.likes, 0);
-	}
+    function testVotePost() public {
+        forum.upVotePost(0);
+        Forum.Post memory _post = forum.getPost(0);
+        assertEq(_post.likes, 1);
+        forum.downVotePost(0);
+        _post = forum.getPost(0);
+        assertEq(_post.likes, 0);
+    }
 }
