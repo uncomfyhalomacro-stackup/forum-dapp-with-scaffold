15e0431354a52e0b17373af4bccaa95a0857fd2c
Soc Virnyl Estela <soc.estela@tribex.co>
2024-08-28 02:50:55 +0000

    change name to ForumScript

--- packages/foundry/script/Forum.s.sol
+++ packages/foundry/script/Forum.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 import {Forum} from "../src/Forum.sol";
 
-contract CounterScript is Script {
+contract ForumScript is Script {
     Forum public forum;
 
     function setUp() public {}
